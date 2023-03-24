ac28612df2311e4ff64213064ff5399edffe20f6
Ayobami <olayoriayobami@gmail.com>
2023-03-24 13:59:43 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  * Return: 0 when successful
  */
 
-int main(void)
+	int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
