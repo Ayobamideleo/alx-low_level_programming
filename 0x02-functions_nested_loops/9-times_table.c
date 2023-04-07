522bfca2484461ae007bac1f64ac7b475b58dc14
Ayobami <olayoriayobami@gmail.com>
2023-04-06 18:58:31 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - function that print times_table
+ *
+ * Return: empty output
+ */
+
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z < 10)
+			{
+				_putchar(z + '0');
+				_putchar('\t');
+			}
+			else
+			{
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+				_putchar('\t');
+			}
+		}
+		_putchar('\n');
+	}
+}
